f57066f04093bd3c0028853e5f58740486e82fc4
Abdon Jimenez <alfadncba@gmail.com>
2026-09-24 14:28:40 -0300

    Enhance dataframe with ImageColumn for 'DN SPARE'
    Added an ImageColumn for 'DN SPARE' in the dataframe display.

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ st.title("Stock Nodo CBA")
 
 url = "https://docs.google.com/spreadsheets/d/1NL2BRRxNlLBL2DJK63DtjLuFDMnbYfHiVktK4pziMSE/edit?gid=0#gid=0"
 
-
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read(spreadsheet=url, ttl=0)
 
 st.dataframe(
     df,
     column_config={
+        "DN SPARE": st.column_config.ImageColumn("Foto"),
         "Link WhatsApp": st.column_config.LinkColumn(
             "WhatsApp",
             display_text="Enviar a WhatsApp"
